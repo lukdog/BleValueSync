6888b986d154e8751f6fb22da0316c14c58a0bd8
Luca Doglione <l.doglione@arduino.cc>
2024-07-13 15:06:15 +0200

    Adding BLE Value Sync library

---
+++ src/BleValueSync.h
@@ -0,0 +1,100 @@
+#ifdef BleValueSync_h
+#define BleValueSync_h
+
+#include <ArduinoBLE.h>
+
+enum ValueType {COUNTER, VALUE};
+
+class BleSyncValue {
+public:
+  BleSyncValue();
+  BleSyncValue(const char *uuid, unsigned int property) {
+    _uuid = uuid;
+    _characteristic = new BLEIntCharacteristic(uuid, property);
+    _characteristic->writeValue(0);
+  }
+
+  void setValue(uint32_t newValue) {
+    if(newValue == value) {return;}
+    value = newValue;
+    _characteristic->writeValue(value);
+  }
+
+private:
+  const char *_uuid;
+  uint32_t value = 0;
+  ValueType _type = VALUE;
+  BLEIntCharacteristic *_characteristic;
+  
+friend class BleSync;
+};
+
+class BleSync {
+public:
+  BleSync(const char *name, const char *uuid, uint8_t valuesQuantity) {
+    _name = name;
+    _uuid = uuid;
+    _values = new BleSyncValue*[valuesQuantity];
+    _quantity = valuesQuantity;
+  }
+
+  bool addValue(BleSyncValue *v){
+    if(_currentQuantity == _quantity ) {
+      return false;
+    }
+
+    _values[_currentQuantity] = v;
+    _currentQuantity++;
+
+    return true;
+    
+  }
+
+  bool initBLE() {
+    _bleService = new BLEService(_uuid);
+    
+    if(!BLE.begin()) {
+      return false;
+    }
+
+    BLE.setLocalName(_name);
+    BLE.setAdvertisedService(*_bleService);
+
+    for(uint8_t i = 0; i<_currentQuantity; i++){
+      _bleService->addCharacteristic(*(_values[i]->_characteristic));
+    }
+
+    BLE.addService(*_bleService);
+    BLE.advertise();
+
+  }
+
+  void sync(unsigned long waitTime){
+    
+    long startSync = millis();
+  
+    while(millis() - startSync < waitTime){
+
+      BLEDevice central = BLE.central();
+
+      if(central){
+        while(central.connected()){
+          delay(1);
+        }
+      }
+
+      delay(100);
+    }
+  }
+
+private:
+  BLEService *_bleService;
+  BleSyncValue **_values;
+  const char *_name;
+  const char *_uuid;
+  uint8_t _quantity = 0;
+  uint8_t _currentQuantity = 0;
+};
+
+#endif
+
